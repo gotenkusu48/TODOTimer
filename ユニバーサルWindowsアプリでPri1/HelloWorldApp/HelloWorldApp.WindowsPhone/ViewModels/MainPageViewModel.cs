323ad0e383e0c2e39ff632641542bcf3d55e482d
gotenkusu48@yahoo.co.jp <gotenkusu48@yahoo.co.jp>
2015-07-05 17:38:37 +0900

    Init()

---
+++ ユニバーサルWindowsアプリでPri1/HelloWorldApp/HelloWorldApp.WindowsPhone/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Practices.Prism.Mvvm;
+using Reactive.Bindings;
+
+namespace HelloWorldApp.ViewModels
+{
+    public class MainPageViewModel : ViewModel
+    {
+        //    private string message = "Hello Prism for Windows Runtime!!";
+
+        //    public string Message
+        //    {
+        //        get { return this.message; }
+        //        set { this.SetProperty(ref this.message, value); }
+        //    }
+        public ReactiveProperty<string> Message { get; set; }
+
+        public MainPageViewModel()
+        {
+            Message = new ReactiveProperty<string>();
+            Message.Value = "Hello World";
+        }
+    }
+}
